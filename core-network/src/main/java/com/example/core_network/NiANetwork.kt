ae8fa0e97f1a16f09d1ab4d0c3343fcfb317c769
Parkduksung <duksung1234@naver.com>
2022-09-09 07:39:58 +0900

    [feature][core-network] NiANetwork interface 내용 추가.

--- core-network/src/main/java/com/example/core_network/NiANetwork.kt
+++ core-network/src/main/java/com/example/core_network/NiANetwork.kt
@@ -1,8 +1,24 @@
 package com.example.core_network
 
+import com.example.core_network.model.NetworkAuthor
+import com.example.core_network.model.NetworkChangeList
+import com.example.core_network.model.NetworkNewsResource
 import com.example.core_network.model.NetworkTopic
 
+/**
+ * Interface representing network calls to the NIA backend
+ */
 interface NiANetwork {
 
     suspend fun getTopics(ids: List<String>? = null): List<NetworkTopic>
+
+    suspend fun getAuthors(ids: List<String>? = null): List<NetworkAuthor>
+
+    suspend fun getNewsResources(ids: List<String>? = null): List<NetworkNewsResource>
+
+    suspend fun getTopicChangeList(after: Int? = null): List<NetworkChangeList>
+
+    suspend fun getAuthorChangeList(after: Int? = null): List<NetworkChangeList>
+
+    suspend fun getNewsResourceChangeList(after: Int? = null): List<NetworkChangeList>
 }
