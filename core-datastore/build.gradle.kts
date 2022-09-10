4a7606287d8bf33ebcdec4a85adda24b4e19828d
Parkduksung <duksung1234@naver.com>
2022-09-10 20:24:48 +0900

    [feature][core-datastore] preference proto 추가.

--- core-datastore/build.gradle.kts
+++ core-datastore/build.gradle.kts
@@ -1,9 +1,13 @@
+import com.google.protobuf.gradle.generateProtoTasks
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
 import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
 
 plugins {
     id("clonenowinandroid.android.library")
     kotlin("kapt")
     id("dagger.hilt.android.plugin")
+    alias(libs.plugins.protobuf)
 
 }
 
@@ -12,6 +16,25 @@ android {
         consumerProguardFiles("consumer-proguard-rules.pro")
     }
 
+}
+
+// Setup protobuf configuration, generating lite Java and Kotlin classes
+protobuf {
+    protoc {
+        artifact = libs.protobuf.protoc.get().toString()
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                val java by registering {
+                    option("lite")
+                }
+                val kotlin by registering {
+                    option("lite")
+                }
+            }
+        }
+    }
 }
 
 dependencies {
---
+++ core-datastore/src/main/java/com/example/core_datastore/NiaPreferences.kt
@@ -0,0 +1,8 @@
+package com.example.core_datastore
+
+import javax.inject.Inject
+
+class NiaPreferences @Inject constructor(
+
+) {
+}
